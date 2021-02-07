9b7a8c825de9e1bef94de93bf9d2fac6e4c23b02
operatorequals <john.torakis@gmail.com>
2021-02-07 23:45:36 +0200

    Fix logging plugin

--- hooks/req_log_hook.py
+++ hooks/req_log_hook.py
@@ -4,9 +4,10 @@ import os, sys
 import tempfile
 
 @hooker.hook("pre_process")  # <-- This declares a hook when a GET request is made
-def req_log_hook(request):
-	log_line = "{method} - '{ua}' - '{url}'".format(
-		method = request.method,
+def req_log_hook(request, url_alias):
+	log_line = "{ip} - {method} - '{ua}' - '{url}'".format(
+		ip = request.remote_addr,
+                method = request.method,
 		ua = request.headers.get("User-Agent", "N/A"),
 		url = request.full_path,
 		)
